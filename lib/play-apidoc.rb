c3b4b832e0e09c70861358565a450b56108639a9
Michael Bryzek <michael@gilt.com>
2015-03-20 15:29:22 -0400

    layout basic lib

---
+++ lib/play-apidoc.rb
@@ -0,0 +1,4 @@
+dir = File.dirname(__FILE__)
+lib_dir = File.join(dir, "play-apidoc")
+
+load File.join(lib_dir, 'args.rb')
---
+++ lib/play-apidoc/args.rb
@@ -0,0 +1,41 @@
+#!/usr/bin/env ruby
+
+module PlayApidoc
+
+  module Args
+
+    # Simple command line argument parsers to avoid pulling in external
+    # dependency. Returns a hash
+    #
+    # Example: ["--organization", "foo"]
+    #  returns: { :organization => "foo" }
+    def Args.parse(values)
+      args = {}
+
+      index = 0
+      while index < values.size do
+        arg = values[index]
+        if md = arg.match(/^\-\-(.+)/)
+          name = md[1].to_s.strip
+          value = values[index+1].to_s.strip
+
+          if value.match(/^\-\-(.+)/)
+            args[name.to_sym] = nil
+          else
+            index += 1
+            if value == ""
+              args[name.to_sym] = nil
+            else
+              args[name.to_sym] = value
+            end
+          end
+        end
+        index += 1
+      end
+
+      args
+    end
+
+  end
+
+end
