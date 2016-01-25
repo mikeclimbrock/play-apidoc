8ddb98f50c1aee145d4dc0678f22118220b5e134
Michael Bryzek <mbryzek@alum.mit.edu>
2016-01-25 08:45:44 -0500

    Use dependency injected controller

--- template/api/app/controllers/Healthchecks.scala
+++ template/api/app/controllers/Healthchecks.scala
@@ -7,7 +7,7 @@ import play.api._
 import play.api.mvc._
 import play.api.libs.json._
 
-object Healthchecks extends Controller {
+class Healthchecks() extends Controller {
 
   private val HealthyJson = Json.toJson(Healthcheck(status = "healthy"))
 
